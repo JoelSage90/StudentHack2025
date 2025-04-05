40ed1dfacffabb818aa675426ea9180b9ac64cdd
Joel Sage <joel.sage@student.manchester.ac.uk>
2025-04-05 18:30:46 +0100

    database set up

--- app.py
+++ app.py
@@ -3,6 +3,57 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("__name__")
 
+#patterning the database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key = True)
+    google_id = db.Column(db.String(100), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer)
+    bio = db.Column(db.String(500))
+    associates = db.relationship('Associates', backref='user', lazy=True)
+    settings = db.relationship('Settings', backref='user', uselist=False)
+    reminders = db.relationship('Reminders', backref='user', lazy=True)
+    def __repr__(self):
+        return f'<User {self.id}>'
+
+class Associates(db.Model):
+    __tablename__ = "associates"
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    name = db.Column(db.String(100), nullable=False)
+    bio = db.Column(db.String(500))
+    voice_id = db.Column(db.String(50))
+    relation_to_user = db.Column(db.String(50))
+
+    def __repr__(self):
+        return f'<Associates {self.id}>'
+
+
+
+class Settings(db.Model):
+    __tablename__ = "settings"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    language = db.Column(db.String(3), default = "en")
+    voice_speed = db.Column(db.Float, default = 1)
+    voice_type = db.Column(db.String(50),default = "Albert")
+    def __repr__(self):
+        return f'<setting {self.id}>'
+    
+class Reminders(db.Model):
+    __tablename__ = "reminders"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    info = db.Column(db.String(200),nullable = False)
+    complete = db.Column(db.Boolean, default = False)
+    def __repr__(self):
+        return f'<Reminder {self.id}>'
+
+#app routing and that
 @app.route("/")
 def index():
     return render_template("login_page.html")
@@ -12,4 +63,6 @@ def home():
     return render_template("home_page.html")
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
