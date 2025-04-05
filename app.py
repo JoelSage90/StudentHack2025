b369f02e08263fd588ee6a02df34c1542f19221b
Joel Sage <joel.sage@student.manchester.ac.uk>
2025-04-05 15:10:20 +0100

    back end begins

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask("__name__")
+
+@app.route("/")
+def index():
+    return render_template("login_page.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
