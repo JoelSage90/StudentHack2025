30fca0302b58ac6792ceb5eb020895e07461dbdd
Joel Sage <joel.sage@student.manchester.ac.uk>
2025-04-05 22:10:26 +0100

    Lord have mercy on me

--- app.py
+++ app.py
@@ -114,4 +114,4 @@ def google_login():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=4000, ssl_context=('cert.crt', 'cert.key'))
+    app.run(debug=True, host='localhost', port=4000, ssl_context=('cert.crt', 'cert.key'))
